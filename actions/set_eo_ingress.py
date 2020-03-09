28752df0c658805691673738caa05baf0f121a35
emyls <emyledirisooriyastudent@gmail.com>
2020-03-09 19:55:43 +0530

    add files

--- actions/set_eo_ingress.py
+++ actions/set_eo_ingress.py
@@ -32,14 +32,19 @@ class CreateEOIngress(Action):
 
     def run(self, namespace, ingressHost):
 
-        self.namespace = namespace
+        self.namespace   = namespace
+        self.ingressHost =  ingressHost
 
         return (True, self._createIngressConfig())
 
-    def _createIngressConfig(self, ingressHost):
+    def _createIngressConfig(self):
         myconf = ingress
         myconf['metadata']['namespace']    = self.namespace
         myconf['spec']['rules'][0]['host'] = self.ingressHost
-        myconf['spec']['rules'][0]['host'] = "environment-operator." + self.namespace + ".bite.pearsondev.tech"
+
+        if self.ingressHost is None:
+            myconf['spec']['rules'][0]['host'] = "environment-operator." + self.namespace + ".bite.pearsondev.tech"
+        else:
+            myconf['spec']['rules'][0]['host'] = self.ingressHost
 
         return myconf
